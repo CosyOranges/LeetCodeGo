fcc2198ccb004111770b17d34c4c7c93a5e1e111
Calvin Davies <calvin.davies@benevolent.ai>
2023-05-31 09:41:14 +0100

    feat(q.80): Add solution for Remove Duplicates From Array II

---
+++ Questions/80-Remove-Duplicates-From-Sorted-Array/removeDuplicatesII.go
@@ -0,0 +1,44 @@
+package dsa
+
+import "fmt"
+
+func removeDuplicatesII(nums []int) int {
+	if len(nums) < 3 {
+		return len(nums)
+	}
+	prev1 := nums[0]
+	prev2 := nums[1]
+
+	k := 2
+
+	for i := 2; i < len(nums); i++ {
+		// fmt.Printf("prev1: %v | prev2: %v | nums[i]: %v | nums[k]: %v\n", prev1, prev2, nums[i], nums[k])
+		if prev1 == prev2 && nums[i] != prev2 {
+			nums[k] = nums[i]
+			prev1 = prev2
+			prev2 = nums[i]
+			k++
+		} else if prev1 != prev2 && nums[i] == prev2 {
+			nums[k] = nums[i]
+			prev1 = prev2
+			prev2 = nums[i]
+			k++
+		} else if prev1 != prev2 && nums[i] != prev2 {
+			nums[k] = nums[i]
+			prev1 = prev2
+			prev2 = nums[i]
+			k++
+		} else {
+			prev1 = prev2
+			prev2 = nums[i]
+		}
+	}
+	// fmt.Printf("Array: %v\n", nums)
+	return k
+}
+
+func main() {
+	nums := []int{1, 1, 1, 2, 2, 3}
+	ans := removeDuplicatesII(nums)
+	fmt.Printf("The answer is: %v\n", ans)
+}
---
+++ Questions/80-Remove-Duplicates-From-Sorted-Array/removeDuplicatesII_test.go
@@ -0,0 +1,39 @@
+package dsa
+
+import "testing"
+
+func TestRemoveDuplicatesII(t *testing.T) {
+	// Set up some test cases
+	nums1 := []int{1, 1, 1, 2, 2, 3}
+	nums2 := []int{1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 4, 5}
+	nums3 := []int{1, 1}
+
+	// Set tests to be run i.e. t.Run("description", func(t *testing.T) {...})
+	t.Run("Test standard arrays", func(t *testing.T) {
+		want := 5
+
+		got := removeDuplicatesII(nums1)
+
+		if got != want {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+
+		want = 6
+
+		got = removeDuplicatesII(nums2)
+
+		if got != want {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	})
+
+	t.Run("Test short arrays", func(t *testing.T) {
+		want := 2
+
+		got := removeDuplicatesII(nums3)
+
+		if got != want {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	})
+}
