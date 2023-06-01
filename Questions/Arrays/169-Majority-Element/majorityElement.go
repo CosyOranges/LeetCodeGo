cebabd1462485ff49bdba5baddeaa0d662b1e1e8
Calvin Davies <calvin.davies@benevolent.ai>
2023-06-01 09:49:56 +0100

    fix(q.169): Optimise to constant space and linear time

--- Questions/Arrays/169-Majority-Element/majorityElement.go
+++ Questions/Arrays/169-Majority-Element/majorityElement.go
@@ -18,6 +18,33 @@ func majorityElement(nums []int) int {
 	return ans
 }
 
+func majorityElementConstantSpace(nums []int) int {
+	// To achieve this without using a map you can use the Boyer-Moore Majority Vote algorithm
+	// This works by:
+	// Assigning a candidate to the first position of the array
+	candidate := nums[0]
+
+	// Assigning a count to keep track of the instances of the candidate
+	count := 1
+
+	for _, num := range nums[1:] {
+		if num == candidate {
+			count++
+		} else {
+			count--
+		}
+		// If the count every reaches 0 then the new candidate is our current index
+		// Which every number is repeated most frequently will be the final candidate
+		// Provided that frequency is greater than len(nums) / 2
+		if count == 0 {
+			candidate = num
+			count = 1
+		}
+	}
+
+	return candidate
+}
+
 func main() {
 	nums := []int{2, 2, 1, 1, 1, 2, 2}
 	ans := majorityElement(nums)
--- Questions/Arrays/169-Majority-Element/majorityElement_test.go
+++ Questions/Arrays/169-Majority-Element/majorityElement_test.go
@@ -15,3 +15,29 @@ func TestMajorityElements(t *testing.T) {
 		}
 	})
 }
+
+func TestMajorityElementConstantSpace(t *testing.T) {
+	nums1 := []int{2, 2, 1, 1, 1, 2, 2}
+	nums2 := []int{1, 1, 2, 2, 3, 3}
+
+	t.Run("Test normal case", func(t *testing.T) {
+		want := 2
+
+		got := majorityElementConstantSpace(nums1)
+
+		if got != want {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	})
+
+	t.Run("Test case with equal number", func(t *testing.T) {
+		want := 3
+
+		// Should return the final element
+		got := majorityElementConstantSpace(nums2)
+
+		if got != want {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	})
+}
