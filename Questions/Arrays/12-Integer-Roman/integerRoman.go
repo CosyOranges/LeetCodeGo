5a62e900328e0d4e8b3a290075a6d62e8440db74
Calvin Davies <calvin.davies@benevolent.ai>
2023-06-14 11:05:38 +0100

    feat(q.12): Add solution to convert integer to roman numeral

---
+++ Questions/Arrays/12-Integer-Roman/integerRoman.go
@@ -0,0 +1,54 @@
+package arrays
+
+import (
+	"strings"
+)
+
+func intToRoman(num int) string {
+	ans := ""
+	if num == 0 {
+		return ans
+	}
+
+	numerals := map[int]string{
+		1:    "I",
+		4:    "IV",
+		5:    "V",
+		9:    "IX",
+		10:   "X",
+		40:   "XL",
+		50:   "L",
+		90:   "XC",
+		100:  "C",
+		400:  "CD",
+		500:  "D",
+		900:  "CM",
+		1000: "M",
+	}
+
+	nums := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	for _, x := range nums {
+		if num >= x {
+			residual := num / x
+			ans += strings.Repeat(numerals[x], residual)
+			num = num % x
+		}
+	}
+	return ans
+}
+
+func intToSlice(num int, arr []int) []int {
+	if num != 0 {
+		i := num % 10
+		arr = append([]int{i}, arr...)
+		return intToSlice(num/10, arr)
+	}
+
+	return arr
+}
+
+// func main() {
+// 	// 3999 -> MMMCMXCIX
+// 	fmt.Printf("Ans: %s\n", intToRoman(3999))
+// 	// fmt.Printf("Mod 4 % 1000: %v\n")
+// }
