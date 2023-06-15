512ca5ab9cece36f2a1757e91ff3d78bbecff8c4
Calvin Davies <calvin.davies@benevolent.ai>
2023-06-15 17:06:54 +0100

    feat(q.68): Add solution for Text Justification

--- Questions/Arrays/12-Integer-Roman/integerRoman.go
+++ Questions/Arrays/12-Integer-Roman/integerRoman.go
@@ -1,6 +1,7 @@
-package arrays
+package main
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -29,8 +30,15 @@ func intToRoman(num int) string {
 	nums := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
 	for _, x := range nums {
 		if num >= x {
+			// Dividing an integer will give floor(x/y) e.g. 3999/1000 = 3
 			residual := num / x
+			fmt.Printf("Res: %v | num: %v | x: %v\n", residual, num, x)
+			// We then want to repeat numerals[x] residual times
+			// e.g. for 3999/1000 = 3, we want to repeat 1000(M) 3 times
+			// for situations like 900, 90, or 9 the residual will only ever be 1
 			ans += strings.Repeat(numerals[x], residual)
+
+			// Finally we take the mod of num % x ==> 3999 % 1000 = 999
 			num = num % x
 		}
 	}
@@ -47,8 +55,8 @@ func intToSlice(num int, arr []int) []int {
 	return arr
 }
 
-// func main() {
-// 	// 3999 -> MMMCMXCIX
-// 	fmt.Printf("Ans: %s\n", intToRoman(3999))
-// 	// fmt.Printf("Mod 4 % 1000: %v\n")
-// }
+func main() {
+	// 3999 -> MMMCMXCIX
+	fmt.Printf("Ans: %s\n", intToRoman(3999))
+	// fmt.Printf("Mod 4 % 1000: %v\n")
+}
