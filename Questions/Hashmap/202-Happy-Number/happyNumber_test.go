7fc4775d5ba3548469f24f6d95159f427bc970bf
Calvin Davies <calvin.davies@benevolent.ai>
2023-06-17 19:29:26 +0100

    fix(q.202): Add test to Happy Number

--- Questions/Hashmap/202-Happy-Number/happyNumber_test.go
+++ Questions/Hashmap/202-Happy-Number/happyNumber_test.go
@@ -1,6 +1,9 @@
 package hashmap
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestIsHappy(t *testing.T) {
 	t.Run("Test unhappy number", func(t *testing.T) {
@@ -12,6 +15,24 @@ func TestIsHappy(t *testing.T) {
 	t.Run("Test happy number", func(t *testing.T) {
 		if !isHappy(19) {
 			t.Errorf("got: want, want: true")
+		}
+	})
+}
+
+func TestGetDigits(t *testing.T) {
+	t.Run("Test single digit", func(t *testing.T) {
+		got := getDigits(2, []int{})
+
+		if !reflect.DeepEqual(got, []int{2}) {
+			t.Errorf("got: %v, want: %v", got, []int{2})
+		}
+	})
+
+	t.Run("Test multiple digit", func(t *testing.T) {
+		got := getDigits(2222, []int{})
+
+		if !reflect.DeepEqual(got, []int{2, 2, 2, 2}) {
+			t.Errorf("got: %v, want: %v", got, []int{2, 2, 2, 2})
 		}
 	})
 }
